ae57570a148012364be8889f205f6f7ecf8d4d64
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2015-03-27 16:48:33 +0200

    Removes unused imports

--- src/test/java/com/thetransactioncompany/jsonrpc2/JSONRPC2ResponseTest.java
+++ src/test/java/com/thetransactioncompany/jsonrpc2/JSONRPC2ResponseTest.java
@@ -1,29 +1,34 @@
 package com.thetransactioncompany.jsonrpc2;
 
+
 import java.util.Map;
 
 import junit.framework.TestCase;
 
-import com.thetransactioncompany.jsonrpc2.JSONRPC2Parser;
-import com.thetransactioncompany.jsonrpc2.JSONRPC2ParseException;
-
 import net.minidev.json.JSONAware;
 
+
 /**
- * Tests the JSON-RPC 2.0 request class.
+ * Tests the JSON-RPC 2.0 response class.
  */
 public class JSONRPC2ResponseTest extends TestCase {
-    public class Bean {
-        protected String test = "foo";
 
-        public String getTest() {
-            return this.test;
-        }
-    }
+
+	public class Bean {
+
+
+		protected String test = "foo";
+
+
+		public String getTest() {
+			return this.test;
+		}
+	}
+
 
 	public void testSetIDFromToString() {
 
-		Object id = new Object(){
+		Object id = new Object() {
 			@Override
 			public String toString() {
 				return "id";
@@ -35,40 +40,40 @@ public class JSONRPC2ResponseTest extends TestCase {
 		assertEquals("id", response.getID());
 	}
 
-    public void testSetResultJsonAware()
-        throws
-            JSONRPC2ParseException
-    {
-        JSONRPC2Parser parser = new JSONRPC2Parser();
 
-        JSONAware result = new JSONAware() {
-            public String toJSONString() {
-                return "\"dummy\"";
-            }
-        };
+	public void testSetResultJsonAware()
+		throws JSONRPC2ParseException {
+
+		JSONRPC2Parser parser = new JSONRPC2Parser();
+
+		JSONAware result = new JSONAware() {
+			public String toJSONString() {
+				return "\"dummy\"";
+			}
+		};
 
 		JSONRPC2Response response = new JSONRPC2Response(result, "id");
-        // we need to check serialized form
-        response = parser.parseJSONRPC2Response(response.toJSONString());
+		// we need to check serialized form
+		response = parser.parseJSONRPC2Response(response.toJSONString());
 
 		assertEquals("dummy", response.getResult());
-    }
+	}
 
-    public void testSetResultBean()
-        throws
-            JSONRPC2ParseException
-    {
-        JSONRPC2Parser parser = new JSONRPC2Parser();
 
-        Object result = new JSONRPC2ResponseTest.Bean();
+	public void testSetResultBean()
+		throws JSONRPC2ParseException {
+		
+		JSONRPC2Parser parser = new JSONRPC2Parser();
+
+		Object result = new JSONRPC2ResponseTest.Bean();
 
 		JSONRPC2Response response = new JSONRPC2Response(result, "id");
-        // we need to check serialized form
-        response = parser.parseJSONRPC2Response(response.toJSONString());
+		// we need to check serialized form
+		response = parser.parseJSONRPC2Response(response.toJSONString());
 
-        result = response.getResult();
-        assertTrue(result instanceof Map);
-        assertTrue(((Map) result).containsKey("test"));
+		result = response.getResult();
+		assertTrue(result instanceof Map);
+		assertTrue(((Map) result).containsKey("test"));
 		assertEquals("foo", ((Map) result).get("test"));
-    }
+	}
 }
